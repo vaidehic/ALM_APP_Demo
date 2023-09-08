84a2af20fba2ff0ef94990e03db64c1d526b43e8
vaidehic <vaidehic@cybage.com>
2023-09-08 18:28:56 +0530

    code added

---
+++ ALM APP Dem0/ALM_APP_Dem0App.swift
@@ -0,0 +1,17 @@
+//
+//  ALM_APP_Dem0App.swift
+//  ALM APP Dem0
+//
+//  Created by administrator on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ALM_APP_Dem0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
